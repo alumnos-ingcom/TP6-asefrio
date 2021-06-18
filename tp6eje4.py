642ac0fcece1074cc7bcf49b4c3c7a40ce9ddbc4
marsiocons <maaarciosk8@gmail.com>
2021-06-17 21:30:36 -0300

    La función ahora lee línea por línea. Usado anagramas.txt como prueba

--- tp6eje4.py
+++ tp6eje4.py
@@ -5,12 +5,32 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-def encriptar_archivo(texto, rotacion):
+#from utilidades import ingreso_entero, IngresoIncorrecto
 
+def encriptar_archivo(n_archivo, rotacion):
+    
+    archivo = open(n_archivo)
+    
+    texto = archivo.readlines()
+    
+    for posicion, linea in enumerate(texto):
+        
+        print(f"({posicion}) > {linea}")
+        
+    archivo.close()
+    
     pass
 
+
 def principal():
-    """Toda la interacción con el usuario va acá"""
+    
+    #archivo = input("Ingrese el nombre del archivo que desea encriptar: ")
+    #rotacion = ingreso_entero("Ingrese la rotación para encriptar: # ")
+    
+    #encriptar_archivo(archivo, rotacion)
+    
+    encriptar_archivo("anagramas.txt", 1)
+    
     pass
 
 if __name__ == "__main__":
