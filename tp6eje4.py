ad17cd48cfe7d035b953bc73a68ca15138764822
marsiocons <maaarciosk8@gmail.com>
2021-06-17 21:02:03 -0300

    Commit inicial

---
+++ tp6eje4.py
@@ -0,0 +1,19 @@
+################
+# Agustin Anhorn - @agusanhorn
+# Tomás Sautú - @TomasSautu
+# Marcio Betanzo - @Marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def encriptar_archivo(texto, rotacion):
+
+    pass
+
+def principal():
+    """Toda la interacción con el usuario va acá"""
+    pass
+
+if __name__ == "__main__":
+    principal()
+
+
