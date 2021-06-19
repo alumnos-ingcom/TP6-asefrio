91b1f849b8c42053fdd2cde61b58e1249008426d
marsiocons <maaarciosk8@gmail.com>
2021-06-19 20:00:34 -0300

    Comit inicial

---
+++ tp6ej6_1.py
@@ -0,0 +1,22 @@
+################
+# Agustin Anhorn - @agusanhorn
+# Tomás Sautú - @TomasSautu
+# Marcio Betanzo - @Marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def hacer_etiquetas(contenido, etiqueta):
+    
+    etiqueta_contenido = f"<{etiqueta}>contenido<{etiqueta}>"
+    
+    return etiqueta_contenido
+
+
+def principal():
+    """Toda la interacción con el usuario va acá"""
+    pass
+
+if __name__ == "__main__":
+    principal()
+
+
