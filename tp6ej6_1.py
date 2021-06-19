a83137c90d2990d3b4372e652b853f270dfba904
marsiocons <maaarciosk8@gmail.com>
2021-06-19 20:21:07 -0300

    Ejercicio terminado?

--- tp6ej6_1.py
+++ tp6ej6_1.py
@@ -7,14 +7,18 @@
 
 def hacer_etiquetas(contenido, etiqueta):
     
-    etiqueta_contenido = f"<{etiqueta}>contenido<{etiqueta}>"
+    etiqueta_contenido = f"<{etiqueta}>contenido</{etiqueta}>"
     
     return etiqueta_contenido
 
 
 def principal():
-    """Toda la interacción con el usuario va acá"""
-    pass
+    
+    etiqueta_mensaje_principal = hacer_etiquetas("Hola HTML", "h1")
+    etiqueta_parrafo = hacer_etiquetas("Esto es un párrafo", "p")    
+    etiqueta_body = hacer_etiquetas(etiqueta_mensaje_principal + etiqueta_parrafo, "body")
+    etiqueta_html = hacer_etiquetas(etiqueta_body, "html")
+    print(etiqueta_html)
 
 if __name__ == "__main__":
     principal()
