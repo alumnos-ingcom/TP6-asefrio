ff745df9a6160651e90df26ce9ef5dbaf3243f3b
agusanhorn <anhornagustin@gmail.com>
2021-06-19 14:09:58 -0300

    Ejercicio 2 Terminado
    Corregí lo que me dijo mi compañero.
    Cambié algunos nombres por más detallados y esta vez si cumple la consigna :P

--- tp6ej2.py
+++ tp6ej2.py
@@ -5,31 +5,37 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-from tp6ej1 import definir_anagrama, quitar_acentos_mayusculas_comas
+from tp6ej1 import definir_anagrama
 
 
 def procesar_archivo():
     archivo = open("anagramas.txt", "r", encoding = "utf-8")
-    lista = []
-    for i in archivo:
-        i.split()
-        texto1 = []
-        texto2 = []
-        a = i.index("-")
-        texto1.append(i[:a])
-        texto2.append(i[a+1:])
-        texto_1 = "".join(texto1)
-        texto_2 = "".join(texto2)
-        r = definir_anagrama(texto_1, texto_2)
-        if r == True:
-            lista.append(r)
-        else:
-            lista.append(r)
-    return lista
+    resultados = ""
+    for linea in archivo:
+        linea.split()
+        texto_1_sin_guion = []
+        texto_2_sin_guion = []
+        guion = linea.index("-")
+        texto_1_sin_guion.append(linea[:guion])
+        texto_2_sin_guion.append(linea[guion+1:])
+        texto_1 = "".join(texto_1_sin_guion)
+        texto_2 = "".join(texto_2_sin_guion)
+        texto_1 = texto_1.strip()
+        texto_2 = texto_2.strip()
+        anagrama = definir_anagrama(texto_1, texto_2)
+        anagrama = str(anagrama)
+        devolver_resultado = anagrama + " " + linea
+        resultados += devolver_resultado
+    return resultados
+        
 
 def principal():
     anagramas = procesar_archivo()
+    anagramas = anagramas.replace("False", "Estos textos no son anagramas entre si -->")
+    anagramas = anagramas.replace("True", "Estos textos son anagramas entre si -->")
     print(anagramas)
+    
+    
     
 if __name__ == "__main__":
     principal()
