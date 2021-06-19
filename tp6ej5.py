440c3b8115d4572d128b03ec9e509ec8b243981b
marsiocons <maaarciosk8@gmail.com>
2021-06-19 18:53:51 -0300

    Comit inicial

---
+++ tp6ej5.py
@@ -0,0 +1,20 @@
+################
+# Agustin Anhorn - @agusanhorn
+# Tomás Sautú - @TomasSautu
+# Marcio Betanzo - @Marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+def desencriptar_archivo(archivo, rotacion):
+    pass
+
+
+def principal():
+    """Toda la interacción con el usuario va acá"""
+    pass
+
+if __name__ == "__main__":
+    principal()
+
+
